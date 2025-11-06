c84e8d2640b4d4579e720a54a26d5ed0e436bd61
BADR EDDINE SLAOUI <slaoui.badr.eddine.sdv@gmail.com>
2025-11-06 13:04:58 +0100

    Update service worker registration path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/GameAPI/service-worker.js")
+    .register("./service-worker.js")
     .then(() => console.log("✅ Service Worker registered"))
     .catch((err) => console.error("❌ SW registration failed:", err));
 }
